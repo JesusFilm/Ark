f4fcafc2b5932c301f2423e752689c56a2f8e17e
Gavin Walsh <walsh.gavin@gmail.com>
2021-04-16 12:14:19 -0400

    author card

---
+++ jesus-film.base/author-card/author-card.composition.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { AuthorCard } from './author-card';
+
+export const MinimalAuthorCard = () => (
+  <AuthorCard 
+    name="Tez Brooks" 
+    description="Award winning author, screenwriter and international speaker" 
+  />
+);
+
+export const FrillsAuthorCard = () => (
+  <AuthorCard 
+    name="Tez Brooks" 
+    description="Award winning author, screenwriter and international speaker" 
+    src="https://www.jesusfilm.org/content/dam/jesusfilm/avatars/tezg.jpg"
+    onClick={() => alert("yoyoyo")} 
+  />
+);
