0a08c7439ba2ec38588cd746a2856676cef3e562
Gavin Walsh <walsh.gavin@gmail.com>
2021-04-20 16:33:00 -0400

    changes

--- jesus-film.base/author-list/author-list.spec.tsx
+++ jesus-film.base/author-list/author-list.spec.tsx
@@ -5,7 +5,7 @@ import { BasicAuthorList } from './author-list.composition';
 describe('author-list', () => {
   it('should render with the correct text', () => {
     const { getByText } = render(<BasicAuthorList />);
-    const rendered = getByText('hello from AuthorList');
+    const rendered = getByText('Tez Brooks');
     expect(rendered).toBeTruthy();
   });
 });
--- jesus-film.base/author-list/author-list.tsx
+++ jesus-film.base/author-list/author-list.tsx
@@ -12,9 +12,9 @@ export type AuthorListProps = {
 
 export function AuthorList({ authors }: AuthorListProps) {
   return (
-    <GridList cellHeight={160} cols={3}>
+    <GridList cellHeight={200} cols={1}>
       {authors.map((author, i) => (
-        <GridListTile key={`${i}-author`} cols={2}>
+        <GridListTile key={`${i}-author`} cols={1}>
           <AuthorCard 
             name={author.name}
             description={author.description}
