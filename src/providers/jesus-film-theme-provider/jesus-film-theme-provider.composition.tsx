fdd0c344150756aa572cebe929dff5a953df4b11
Tataihono Nikora <tataihono.nikora@gmail.com>
2021-04-28 16:23:58 +1200

    update provider

--- src/providers/jesus-film-theme-provider/jesus-film-theme-provider.composition.tsx
+++ src/providers/jesus-film-theme-provider/jesus-film-theme-provider.composition.tsx
@@ -1,11 +1,11 @@
+import { Typography } from '@material-ui/core'
 import React from 'react'
-import { CompleteHero } from '@jesus-film/ark.elements.hero/hero.composition'
 import { JesusFilmThemeProvider } from './jesus-film-theme-provider'
 
 export const BasicJesusFilmThemeProvider = () => {
   return (
     <JesusFilmThemeProvider>
-      <CompleteHero />
+      <Typography>Hello World</Typography>
     </JesusFilmThemeProvider>
   )
 }
--- src/providers/voke-theme-provider/voke-theme-provider.composition.tsx
+++ src/providers/voke-theme-provider/voke-theme-provider.composition.tsx
@@ -1,11 +1,11 @@
+import { Typography } from '@material-ui/core'
 import React from 'react'
-import { CompleteHero } from '@jesus-film/ark.elements.hero/hero.composition'
 import { VokeThemeProvider } from './voke-theme-provider'
 
 export const BasicVokeThemeProvider = () => {
   return (
     <VokeThemeProvider>
-      <CompleteHero />
+      <Typography>Hello World</Typography>
     </VokeThemeProvider>
   )
 }
