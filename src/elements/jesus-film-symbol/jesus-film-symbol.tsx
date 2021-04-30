9815874434b570032c009aa8f6c02006ec068dc8
Tataihono Nikora <tataihono.nikora@gmail.com>
2021-04-30 18:11:27 +1200

    add brandmarks and symbols

---
+++ src/elements/jesus-film-symbol/jesus-film-symbol.tsx
@@ -0,0 +1,32 @@
+import React, { ImgHTMLAttributes } from 'react'
+import Red from './assets/red.png'
+import CoolGray from './assets/cool-gray.png'
+import WarmWhite from './assets/warm-white.png'
+import White from './assets/white.png'
+
+export type JesusFilmSymbolProps = {
+  variant?: 'red' | 'cool-gray' | 'warm-white' | 'white'
+} & ImgHTMLAttributes<HTMLImageElement>
+
+export function JesusFilmSymbol ({ variant = 'red', ...rest }: JesusFilmSymbolProps) {
+  let source: string
+
+  switch (variant) {
+    case 'red':
+      source = Red
+      break
+    case 'cool-gray':
+      source = CoolGray
+      break
+    case 'warm-white':
+      source = WarmWhite
+      break
+    case 'white':
+      source = White
+      break
+  }
+
+  return (
+    <img src={source} {...rest} />
+  )
+}
