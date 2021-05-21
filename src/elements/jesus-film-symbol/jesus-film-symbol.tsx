6393b4e6c00f733413135b5c55e4437fbcb99714
Tataihono Nikora <tataihono.nikora@gmail.com>
2021-05-21 15:48:03 +1200

    update symbol

--- src/elements/jesus-film-symbol/jesus-film-symbol.tsx
+++ src/elements/jesus-film-symbol/jesus-film-symbol.tsx
@@ -1,31 +1,39 @@
 import React, { ImgHTMLAttributes } from 'react'
 import Red from './assets/red.png'
+import RedText from './assets/red-text.svg'
 import CoolGray from './assets/cool-gray.png'
+import CoolGrayText from './assets/cool-gray-text.svg'
 import WarmWhite from './assets/warm-white.png'
+import WarmWhiteText from './assets/warm-white-text.svg'
 import White from './assets/white.png'
+import WhiteText from './assets/white-text.svg'
 
 export type JesusFilmSymbolProps = {
+  /** logo primary color */
   variant?: 'red' | 'cool-gray' | 'warm-white' | 'white'
+  /** show Jesus text on logo */
+  text?: boolean
 } & ImgHTMLAttributes<HTMLImageElement>
 
 export function JesusFilmSymbol({
   variant = 'red',
+  text,
   ...rest
 }: JesusFilmSymbolProps) {
   let source: string
 
   switch (variant) {
     case 'red':
-      source = Red
+      source = text ? RedText : Red
       break
     case 'cool-gray':
-      source = CoolGray
+      source = text ? CoolGrayText : CoolGray
       break
     case 'warm-white':
-      source = WarmWhite
+      source = text ? WarmWhiteText : WarmWhite
       break
     case 'white':
-      source = White
+      source = text ? WhiteText : White
       break
   }
 
