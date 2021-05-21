8990058a6b1794aa0716790c07ceb75995c81602
Tataihono Nikora <tataihono.nikora@gmail.com>
2021-05-21 15:57:36 +1200

    update composition

--- src/elements/jesus-film-graphic-seal/jesus-film-graphic-seal.composition.tsx
+++ src/elements/jesus-film-graphic-seal/jesus-film-graphic-seal.composition.tsx
@@ -1,12 +1,21 @@
+import { Box } from '@material-ui/core'
 import React from 'react'
 import { JesusFilmGraphicSeal } from './jesus-film-graphic-seal'
 
-export const BasicJesusFilmGraphicSeal = () => <JesusFilmGraphicSeal />
+export const BasicJesusFilmGraphicSeal = () => (
+  <Box p={5}>
+    <JesusFilmGraphicSeal />
+  </Box>
+)
 
 export const RedJesusFilmGraphicSeal = () => (
-  <JesusFilmGraphicSeal variant="red" />
+  <Box p={5}>
+    <JesusFilmGraphicSeal variant="red" />
+  </Box>
 )
 
 export const WhiteJesusFilmGraphicSeal = () => (
-  <JesusFilmGraphicSeal variant="white" />
+  <Box p={5} style={{ backgroundColor: '#ef3340' }}>
+    <JesusFilmGraphicSeal variant="white" />
+  </Box>
 )
