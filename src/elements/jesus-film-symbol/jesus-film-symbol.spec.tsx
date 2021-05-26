b5b70f7cdcfa5d921880dd1fcb396c8148545a5f
Tataihono Nikora <tataihono.nikora@gmail.com>
2021-05-27 10:17:20 +1200

    ARK-8 add graphic seal and allow symbol to show "Jesus" (#14)

--- src/elements/jesus-film-symbol/jesus-film-symbol.spec.tsx
+++ src/elements/jesus-film-symbol/jesus-film-symbol.spec.tsx
@@ -19,12 +19,28 @@ describe('jesus-film-symbol', () => {
     ).toEqual('red.png')
   })
 
+  it('should render with red symbol and Jesus text', () => {
+    const { getByRole } = render(<JesusFilmSymbol variant="red" text />)
+
+    expect(
+      getByRole('img', { name: 'jesus-film-symbol' }).getAttribute('src')
+    ).toEqual('red-text.svg')
+  })
+
   it('should render with cool-gray symbol', () => {
     const { getByRole } = render(<JesusFilmSymbol variant="cool-gray" />)
 
     expect(
       getByRole('img', { name: 'jesus-film-symbol' }).getAttribute('src')
     ).toEqual('cool-gray.png')
+  })
+
+  it('should render with cool-gray symbol and Jesus text', () => {
+    const { getByRole } = render(<JesusFilmSymbol variant="cool-gray" text />)
+
+    expect(
+      getByRole('img', { name: 'jesus-film-symbol' }).getAttribute('src')
+    ).toEqual('cool-gray-text.svg')
   })
 
   it('should render with warm-white symbol', () => {
@@ -35,6 +51,14 @@ describe('jesus-film-symbol', () => {
     ).toEqual('warm-white.png')
   })
 
+  it('should render with warm-white symbol and Jesus text', () => {
+    const { getByRole } = render(<JesusFilmSymbol variant="warm-white" text />)
+
+    expect(
+      getByRole('img', { name: 'jesus-film-symbol' }).getAttribute('src')
+    ).toEqual('warm-white-text.svg')
+  })
+
   it('should render with white symbol', () => {
     const { getByRole } = render(<JesusFilmSymbol variant="white" />)
 
@@ -42,4 +66,12 @@ describe('jesus-film-symbol', () => {
       getByRole('img', { name: 'jesus-film-symbol' }).getAttribute('src')
     ).toEqual('white.png')
   })
+})
+
+it('should render with white symbol and Jesus text', () => {
+  const { getByRole } = render(<JesusFilmSymbol variant="white" text />)
+
+  expect(
+    getByRole('img', { name: 'jesus-film-symbol' }).getAttribute('src')
+  ).toEqual('white-text.svg')
 })
