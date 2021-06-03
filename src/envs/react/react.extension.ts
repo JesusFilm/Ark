c1323c0e99937ceab062597ff1335046050c770d
Tataihono Nikora <tataihono.nikora@gmail.com>
2021-06-03 17:10:01 +1200

    require resolve

--- src/envs/react/react.extension.ts
+++ src/envs/react/react.extension.ts
@@ -1,13 +1,14 @@
 import { EnvsMain, EnvsAspect } from '@teambit/envs'
 import { ReactAspect, ReactMain } from '@teambit/react'
 import { Aspect } from '@teambit/harmony'
-import jestConfig from './jest/jest.config'
 
 export class ReactExtension {
   static dependencies: Aspect[] = [EnvsAspect, ReactAspect]
 
   static async provider([envs, react]: [EnvsMain, ReactMain]) {
-    const reactEnv = react.compose([react.overrideJestConfig(jestConfig)])
+    const reactEnv = react.compose([
+      react.overrideJestConfig(require.resolve('./jest/jest.config'))
+    ])
 
     envs.registerEnv(reactEnv)
 
