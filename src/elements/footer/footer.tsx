bd15d1e8e6cbe66a3d14ad867bf32b961ff681d4
Tataihono Nikora <tataihono.nikora@gmail.com>
2021-06-14 15:04:11 +1200

    add spacing and paper

--- src/elements/footer/footer.tsx
+++ src/elements/footer/footer.tsx
@@ -7,7 +7,9 @@ import {
   useTheme,
   Grid,
   Typography,
-  makeStyles
+  makeStyles,
+  Container,
+  Paper
 } from '@material-ui/core'
 import React, { useState } from 'react'
 import convertToTree from '@jesus-film/ark.util.convert-to-tree'
@@ -70,6 +72,9 @@ function ListItem({ node, level = 0 }: ListItemProp) {
 }
 
 const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(5, 0, 7)
+  },
   about: {
     lineHeight: '2rem'
   }
@@ -92,70 +97,77 @@ export function Footer({ menus }: FooterProps) {
   const classes = useStyles()
 
   return (
-    <Grid container spacing={2}>
-      {menus?.map((menu) => (
-        <Grid item key={menu.id} xs>
-          <List
-            subheader={
-              <ListSubheader disableSticky>{menu.name}</ListSubheader>
-            }>
-            {convertToTree(menu.menuItems.nodes).map((node) => (
-              <ListItem key={node.id} node={node} />
-            ))}
-          </List>
+    <Paper elevation={0} className={classes.paper}>
+      <Container>
+        <Grid container spacing={2}>
+          {menus?.map((menu) => (
+            <Grid item key={menu.id} xs>
+              <List
+                subheader={
+                  <ListSubheader disableSticky>{menu.name}</ListSubheader>
+                }>
+                {convertToTree(menu.menuItems.nodes).map((node) => (
+                  <ListItem key={node.id} node={node} />
+                ))}
+              </List>
+            </Grid>
+          ))}
+          <Grid item xs>
+            <List
+              subheader={<ListSubheader disableSticky>About</ListSubheader>}>
+              <MaterialListItem>
+                <ListItemText
+                  primary={
+                    <Typography className={classes.about}>
+                      100 Lake Hart Dr
+                      <br />
+                      Orlando, FL 32832
+                    </Typography>
+                  }
+                />
+              </MaterialListItem>
+              <MaterialListItem>
+                <ListItemText
+                  primary={
+                    <Typography className={classes.about}>
+                      Office: (407) 826-2300
+                      <br />
+                      Fax: (407) 826-2375
+                    </Typography>
+                  }
+                />
+              </MaterialListItem>
+              <MaterialListItem>
+                <ListItemText
+                  primary={
+                    <Typography className={classes.about}>
+                      <span>
+                        Copyright &copy; 1995-{new Date().getFullYear()}.
+                      </span>
+                      <br />
+                      Jesus Film Project&reg;.
+                      <br />
+                      All rights reserved
+                    </Typography>
+                  }
+                />
+              </MaterialListItem>
+            </List>
+          </Grid>
+          <Grid item xs={12} container spacing={10} justify="center">
+            <Grid item>
+              <a href="https://jesusfilm.org" target="_blank" rel="noreferrer">
+                <img src={JesusFilmLogo} height={90} />
+              </a>
+            </Grid>
+            <Grid item>
+              <a href="https://cru.org" target="_blank" rel="noreferrer">
+                <img src={CruLogo} height={90} />
+              </a>
+            </Grid>
+          </Grid>
         </Grid>
-      ))}
-      <Grid item xs>
-        <List subheader={<ListSubheader disableSticky>About</ListSubheader>}>
-          <MaterialListItem>
-            <ListItemText
-              primary={
-                <Typography className={classes.about}>
-                  100 Lake Hart Dr
-                  <br />
-                  Orlando, FL 32832
-                </Typography>
-              }
-            />
-          </MaterialListItem>
-          <MaterialListItem>
-            <ListItemText
-              primary={
-                <Typography className={classes.about}>
-                  Office: (407) 826-2300
-                  <br />
-                  Fax: (407) 826-2375
-                </Typography>
-              }
-            />
-          </MaterialListItem>
-          <MaterialListItem>
-            <ListItemText
-              primary={
-                <Typography className={classes.about}>
-                  <span>Copyright &copy; 1995-{new Date().getFullYear()}.</span>
-                  <br />
-                  Jesus Film Project&reg;.
-                  <br />
-                  All rights reserved
-                </Typography>
-              }
-            />
-          </MaterialListItem>
-        </List>
-      </Grid>
-      <Grid item xs={12} container spacing={10} justify="center">
-        <Grid item>
-          <a href="https://jesusfilm.org" target="_blank" rel="noreferrer">
-            <img src={JesusFilmLogo} height={90} />
-          </a>
-        </Grid>
-        <Grid item>
-          <a href="https://cru.org" target="_blank" rel="noreferrer">
-            <img src={CruLogo} height={90} />
-          </a>
-        </Grid>
-      </Grid>
-    </Grid>
+      </Container>
+    </Paper>
   )
 }
