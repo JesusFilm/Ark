d8c464ac4608fd924e49adcb991745cf099f31d4
Tataihono Nikora <tataihono.nikora@gmail.com>
2021-06-14 15:09:35 +1200

    add paper and container to footer (#21)

--- src/elements/footer/footer.tsx
+++ src/elements/footer/footer.tsx
@@ -5,14 +5,19 @@ import {
   ListItemText,
   Grid,
   Typography,
-  makeStyles
+  makeStyles,
+  Container,
+  Paper
 } from '@material-ui/core'
 import React from 'react'
 import JesusFilmLogo from './assets/jesus-film-logo.svg'
 import CruLogo from './assets/cru-logo.svg'
 import { Menu, MenuProps } from '@jesus-film/ark.elements.menu'
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(5, 0, 7)
+  },
   about: {
     lineHeight: '2rem'
   }
@@ -24,58 +29,65 @@ export function Footer({ menus }: FooterProps) {
   const classes = useStyles()
 
   return (
-    <Menu menus={menus} direction="row">
-      <Grid item xs>
-        <List subheader={<ListSubheader disableSticky>About</ListSubheader>}>
-          <ListItem>
-            <ListItemText
-              primary={
-                <Typography className={classes.about}>
-                  100 Lake Hart Dr
-                  <br />
-                  Orlando, FL 32832
-                </Typography>
-              }
-            />
-          </ListItem>
-          <ListItem>
-            <ListItemText
-              primary={
-                <Typography className={classes.about}>
-                  Office: (407) 826-2300
-                  <br />
-                  Fax: (407) 826-2375
-                </Typography>
-              }
-            />
-          </ListItem>
-          <ListItem>
-            <ListItemText
-              primary={
-                <Typography className={classes.about}>
-                  <span>Copyright &copy; 1995-{new Date().getFullYear()}.</span>
-                  <br />
-                  Jesus Film Project&reg;.
-                  <br />
-                  All rights reserved
-                </Typography>
-              }
-            />
-          </ListItem>
-        </List>
-      </Grid>
-      <Grid item xs={12} container spacing={10} justify="center">
-        <Grid item>
-          <a href="https://jesusfilm.org" target="_blank" rel="noreferrer">
-            <img src={JesusFilmLogo} height={90} />
-          </a>
-        </Grid>
-        <Grid item>
-          <a href="https://cru.org" target="_blank" rel="noreferrer">
-            <img src={CruLogo} height={90} />
-          </a>
-        </Grid>
-      </Grid>
-    </Menu>
+    <Paper elevation={0} className={classes.paper}>
+      <Container>
+        <Menu menus={menus} direction="row">
+          <Grid item xs>
+            <List
+              subheader={<ListSubheader disableSticky>About</ListSubheader>}>
+              <ListItem>
+                <ListItemText
+                  primary={
+                    <Typography className={classes.about}>
+                      100 Lake Hart Dr
+                      <br />
+                      Orlando, FL 32832
+                    </Typography>
+                  }
+                />
+              </ListItem>
+              <ListItem>
+                <ListItemText
+                  primary={
+                    <Typography className={classes.about}>
+                      Office: (407) 826-2300
+                      <br />
+                      Fax: (407) 826-2375
+                    </Typography>
+                  }
+                />
+              </ListItem>
+              <ListItem>
+                <ListItemText
+                  primary={
+                    <Typography className={classes.about}>
+                      <span>
+                        Copyright &copy; 1995-{new Date().getFullYear()}.
+                      </span>
+                      <br />
+                      Jesus Film Project&reg;.
+                      <br />
+                      All rights reserved
+                    </Typography>
+                  }
+                />
+              </ListItem>
+            </List>
+          </Grid>
+          <Grid item xs={12} container spacing={10} justify="center">
+            <Grid item>
+              <a href="https://jesusfilm.org" target="_blank" rel="noreferrer">
+                <img src={JesusFilmLogo} height={90} />
+              </a>
+            </Grid>
+            <Grid item>
+              <a href="https://cru.org" target="_blank" rel="noreferrer">
+                <img src={CruLogo} height={90} />
+              </a>
+            </Grid>
+          </Grid>
+        </Menu>
+      </Container>
+    </Paper>
   )
 }
