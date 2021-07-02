9b0b57855607364abcbaf8e15728fd11466e96fd
John Geronimo <johnageronimo1@gmail.com>
2021-07-02 16:17:15 +1200

    Resolved post test bug

--- src/elements/post/post.spec.tsx
+++ src/elements/post/post.spec.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import { BasicPost } from './post.composition'
+import { I18nProvider } from '../../providers/i18n-provider'
 
 it('should render with the correct text', () => {
-  const { getByText } = render(<BasicPost />)
-  expect(getByText('Jesus Film Project')).toBeInTheDocument()
+  const { getByText } = render(
+    <I18nProvider>
+      <BasicPost />
+    </I18nProvider>
+  )
+  expect(getByText('The parables from Matthew')).toBeInTheDocument()
 })
