3b1a6b47918a14d51e87643a92293ba94c6062eb
John Geronimo <johnageronimo1@gmail.com>
2021-07-05 08:25:54 +1200

    rewired to the right file imports

--- src/elements/post/post.spec.tsx
+++ src/elements/post/post.spec.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import { BasicPost } from './post.composition'
-import { I18nProvider } from '../../providers/i18n-provider'
+import { I18nProvider } from '@jesus-film/ark.providers.i18n-provider'
 
+// Write more test that check if data is coming our from the different components
 it('should render with the correct text', () => {
   const { getByText } = render(
     <I18nProvider>
