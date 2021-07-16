9696402472c17420f744330882d49d7c0c76c278
John Geronimo <johnageronimo1@gmail.com>
2021-07-16 15:27:55 +1200

    Wrote a test to handle onSeeAllClick

--- src/elements/list-header/list-header.spec.tsx
+++ src/elements/list-header/list-header.spec.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { fireEvent, render } from '@testing-library/react'
-import { BasicListHeader } from './list-header.composition'
+import {
+  BasicListHeader,
+  ListHeaderWithCallback
+} from './list-header.composition'
 
 describe('list-header', () => {
   it('should render the title of the list', () => {
@@ -8,18 +11,18 @@ describe('list-header', () => {
     expect(getByText('Authors')).toBeInTheDocument()
   })
 
-  it('should render SEE ALL if there is a callback', () => {
-    const { getByText } = render(<BasicListHeader />)
+  it('should render SEE ALL text', () => {
+    const { getByText } = render(<ListHeaderWithCallback />)
     expect(getByText('SEE ALL')).toBeInTheDocument()
   })
 
   it('should handle onSeeAllClick', () => {
     const handleClick = jest.fn()
     const { getByText } = render(
-      <BasicListHeader onSeeAllClick={handleClick} />
+      <ListHeaderWithCallback onSeeAllClick={handleClick} />
     )
     fireEvent.click(getByText('SEE ALL'))
     expect(handleClick).toHaveBeenCalled()
-    expect(getByText('SA')).toBeDefined()
+    expect(getByText('SEE ALL')).toBeDefined()
   })
 })
