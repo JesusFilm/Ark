4472dccaa51c10742cf47b4e26952328a634bacc
John Geronimo <johnageronimo1@gmail.com>
2021-07-19 13:58:38 +1200

    Refactor test of core-arclight

--- src/elements/block/core-arclight/core-arclight.spec.tsx
+++ src/elements/block/core-arclight/core-arclight.spec.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import { BasicCoreArclight } from './core-arclight.composition'
+import { CoreArclight } from '.'
 
 it('should render the Arclight video', () => {
-  const { getByTestId } = render(<BasicCoreArclight />)
+  const { getByTestId } = render(
+    <CoreArclight
+      __typename="LazyblockArclightBlock"
+      attributes={{
+        refId: '1_1106-mld-0-0'
+      }}
+    />
+  )
   expect(getByTestId('Arclight').getAttribute('src')).toEqual(
     'http://api.arclight.org/videoPlayerUrl?refId=1_1106-mld-0-0'
   )
