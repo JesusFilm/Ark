4b340fdba5271252a5bea9d8d2639d95ac59eda5
John Geronimo <johnageronimo1@gmail.com>
2021-07-19 12:07:21 +1200

    Add JesusFilmThemeProvider to Mission composition

--- src/elements/mission/mission.composition.tsx
+++ src/elements/mission/mission.composition.tsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import { JesusFilmThemeProvider } from '@jesus-film/ark.providers.jesus-film-theme-provider'
 import { Mission } from './mission'
 
-export const BasicMission = () => <Mission />
+export const BasicMission = () => (
+  <JesusFilmThemeProvider>
+    <Mission />
+  </JesusFilmThemeProvider>
+)
--- src/elements/mission/mission.tsx
+++ src/elements/mission/mission.tsx
@@ -30,7 +30,7 @@ export function Mission() {
     <>
       <Divider className={classes.firstDivider} variant="middle" />
       <Divider className={classes.secondDivider} variant="middle" />
-      <Container maxWidth="md">
+      <Container>
         <Typography className={classes.textColor} variant="h5" align="center">
           {t(
             "Together, we will share Jesus with 5 billion people by the end of 2025, This is where you can read and share stories of how God's amazing love and power is reaching people thorugh churches, organizations, Bible studies, technology and more."
