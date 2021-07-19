0f34635787c3143b7d2eeb2915ee006e0465a1f4
John Geronimo <johnageronimo1@gmail.com>
2021-07-19 12:46:57 +1200

    Refactor test of list-header component

--- src/elements/list-header/list-header.composition.tsx
+++ src/elements/list-header/list-header.composition.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import { ListHeader, ListHeaderProps } from './list-header'
+import { ListHeader } from './list-header'
 
 export const BasicListHeader = () => <ListHeader title="Authors" />
 
-export const ListHeaderWithSeeAll = (props: Partial<ListHeaderProps>) => (
+export const ListHeaderWithSeeAll = () => (
   <ListHeader
     title="Authors"
     onSeeAllClick={() => alert('This is a test click')}
-    {...props}
   />
 )
