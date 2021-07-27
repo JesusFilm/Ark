c283cb0c047f2ed086598b94379869056ec8342a
John Geronimo <johnageronimo1@gmail.com>
2021-07-27 14:16:01 +1200

    Add test for Author-Link

--- src/compounds/author-list/author-list.spec.tsx
+++ src/compounds/author-list/author-list.spec.tsx
@@ -28,11 +28,45 @@ describe('author-list', () => {
             }
           ]
         }}
-        title=""
       />
     )
     expect(getByText('Tez Brooks')).toBeInTheDocument()
     expect(queryByText('Authors')).not.toBeInTheDocument()
+  })
+
+  it('should render author link', () => {
+    const { getAllByTestId } = render(
+      <AuthorList
+        authors={{
+          node: [
+            {
+              name: 'Tez Brooks',
+              slug: 'tez-brooks',
+              avatar: {
+                url: 'https://www.jesusfilm.org/content/dam/jesusfilm/avatars/tezg.jpg'
+              },
+              description:
+                'Award winning author, screenwriter and international speaker'
+            },
+            {
+              name: 'Josh McDowell',
+              slug: 'josh-mcdowell',
+              avatar: {
+                url: 'https://www.jesusfilm.org/content/dam/jesusfilm/avatars/josh-mcdowell.jpg'
+              },
+              description:
+                'On staff with Cru as trailblazer for truth, Josh has been at the forefront of cultural trends and ground-breaking ministry for over five decades.'
+            }
+          ]
+        }}
+        AuthorLink={({ children, href, className }) => (
+          <a href={href} data-testid="author-link" className={className} />
+        )}
+      />
+    )
+    const link = getAllByTestId('author-link')
+    expect(link[0]).toBeInTheDocument()
+    expect(link[0]).toHaveAttribute('href', 'tez-brooks')
   })
 
   it('should render the list header', () => {
